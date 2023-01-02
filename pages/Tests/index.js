d9d2c0c1ee5815fc7bad0a979b33c2e6a640a7f0
Ali Jafari <ali.jafari20@gmail.com>
2023-01-03 01:12:55 +0330

    minor prgrs

--- pages/Tests/index.js
+++ pages/Tests/index.js
@@ -1,23 +1,23 @@
 import useSWR from "swr";
 import Link from "next/link";
+import * as TestApi from '../../api/apiTests';
 
 
 const fetcher = async () => {
-    const response = await fetch('http://localhost:8001/api/v1/tests/');
-    const data = await response.json();
+    const {data} = await TestApi.GetAllTests();
     return data;
 }
 
 
 export default function Test(){
     const {data , error} = useSWR('tests', fetcher);
-    if (error) return <h1>Something went wrong!</h1> 
-    if (!data) return <h1>Something went wrong!</h1>
+
+    
     return (
         <>
             <div className="flex flex-row-reverse pt-20 pr-10">
                 {
-                    data.map(d => {
+                    data?.map(d => {
                         return (
                             <div key={d.name} className=" p-4 border-2  rounded-full hover:bg-gray-200 ">
                                 <Link href={`/Tests/${d.name}`}>
